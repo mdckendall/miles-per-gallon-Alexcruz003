fe24bb7acb0add032d063be636929df7134c84c9
Alexcruz003 <137026458+Alexcruz003@users.noreply.github.com>
2023-07-11 02:04:13 +0000

    This is attempt number 2.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-miles_driven= input ("How many miles have been driven?")
+miles_driven= input ("How many miles have been driven? ")
 miles_driven= float(miles_driven)
-gallons_used= input("How many gallons have been used?")
+gallons_used= input("How many gallons have been used? ")
 gallons_used= float(gallons_used)
 miles_pergallon = miles_driven*gallons_used
 print ( miles_pergallon)
